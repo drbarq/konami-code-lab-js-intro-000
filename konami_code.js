3db5f56d6b98914014bd0f39f8a7753a8d87305a
Joe Tustin <j.tustin@gmail.com>
2019-02-26 20:34:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -62,41 +62,25 @@ const codes = [
 ];
 
 var log = [];
-/*
+
+function init() {
 document.addEventListener('keydown', (e) => {
 	if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight" || e.key === "a" || e.key === "b") {
   	log.push(`${e.key}`);
     if (log.length > 10) {
     log.shift();
       }
+    if(log === codes) {
+        alert("Code Unlocked")
+      }
     }
     else {
       log = []
     }
 });
-*/
-
-document.addEventListener('keydown', (e) function( {
-	if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight" || e.key === "a" || e.key === "b") {
-  	log.push(`${e.key}`);
-    if (log.length > 10) {
-    log.shift();
-      }
-    if (log === codes) {
-      alert("Code Unlocked");
-        }
-    }
-    else {
-      log = []
-    }
-}));
-
-
-/*
-function init() {
 
 }
-*/
+
 
 
 
