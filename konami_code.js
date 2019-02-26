25a3079cb83858320d7f2d5218a21a4090e6f7e9
Joe Tustin <j.tustin@gmail.com>
2019-02-26 20:56:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -72,7 +72,7 @@ function init() {
 
 
 document.addEventListener('keydown', (e) => {
-	if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight" || e.key === "a" || e.key === "b") {
+	if (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "a" || e.key == "b") {
   	log.push(`${e.key}`);
     if (log.length > 10) {
     log.shift();
